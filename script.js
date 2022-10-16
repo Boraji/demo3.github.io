4ff068fee0cffe4d52094efb026292eef534ca7f
Boraji <105603207+Boraji@users.noreply.github.com>
2022-10-16 11:42:33 +0530

    golu

---
+++ script.js
@@ -0,0 +1,8 @@
+var main=document.getElementById("body");
+main.onmouseover=function(){
+    var raja=[]
+    raja[0]=document.getElementById("body");
+    raja[0].style.backgroundColor="red";
+}
+
+        
